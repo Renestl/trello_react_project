1aede420c5f7686a478e20b30d5657aca7f2f5bf
Renestl <renestl@gmail.com>
2017-06-09 17:07:07 -0500

    created card component

--- src/components.jsx
+++ src/components.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
 /* Define Card Component Here*/
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="title">Take React Course</div>
+                <div className="description">code on code on code on code...</div>
+                <div className="actions">
+                    <button className="delete-card">X</button>
+                    <button className="complete-card">></button>
+                </div>
+            </div>
+        )
+    }
+}
 
 /* Column Component */
 class CardColumn extends React.Component {
     render() {
-        return (<div></div>);
+        return (
+            <div>
+                <Card/>
+            </div>
+        );
     }
 }
 
